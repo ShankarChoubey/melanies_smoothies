bf69357d094df6689aee07560a845a0565ac7d1f
ShankarChoubey <shankar.choubey@genpact.com>
2025-03-19 06:59:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,8 +40,8 @@ if ingredients_list:
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
         # st.write (ingredients_string)
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-    values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+         values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
     # st.write(my_insert_stmt)
     #st.stop()
     time_to_start = st.button('Submit Order')
